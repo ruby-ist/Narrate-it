b52863a3259ca56d20147c212273cf3f5dbadbc2
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2021-09-26 13:42:47 +0530

    meh

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   has_many :comments
 
   validates :title, presence: true, length: {minimum: 3, maximum: 25}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-
+  has_many :posts
+  validates :username, presence: true, uniqueness: true, length: {minimum: 4, maximum: 25}
+  validates :email, presence: true, uniqueness: true, format: {with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
 end
