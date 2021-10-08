1ad0df69395f82c549cd1de800397004a3335d75
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2021-10-08 23:25:39 +0530

    "Finished-IV"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 9).order(created_At: :desc)
+    @posts = Post.paginate(page: params[:page], per_page: 9).order(created_at: :desc)
   end
 
   def show
