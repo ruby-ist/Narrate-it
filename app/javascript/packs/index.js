e3045e2a0abe1dd611f3c5ed14112c3d9b28890f
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2021-10-08 19:32:25 +0530

    "Made Responsive"

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,15 +1,31 @@
 $('document').ready(function() {
 	function navShadow() {
 		let scrolled = $(window).scrollTop();
-		if (scrolled > 10){
+		if (scrolled > 10) {
 			$('.nav').addClass('shadow');
-		}
-		else{
+		} else {
 			$('.nav').removeClass('shadow');
 		}
 	}
 	
-	setInterval(navShadow,100);
+	setInterval(navShadow, 100);
 	
+	function navbar() {
+		let sandwich = $('.sandwich2');
+		$('.sandwich').on('click', function () {
+			$('.nav-items').addClass('move');
+			$('.screen').css('display', 'block');
+			sandwich.addClass('cross');
+			sandwich.text("x");
+		});
+		
+		$('.screen, .sandwich2').on('click', function () {
+			$('.nav-items').removeClass('move');
+			$('.screen').css('display', 'none');
+			sandwich.removeClass('cross');
+			sandwich.text("");
+		});
+	}
 	
+	setInterval(navbar, 10);
 });
